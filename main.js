12cc868a1695b5984fa1d9a49a350a177dea637e
s4151128 <s4151128@student.rmit.edu.au>
2026-08-12 14:58:25 +1000

    add audio init
    add an event listener on dialog close to start the audio system

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-// browser loads html > browser loads js > open the dialog > user close dialog > audio system loads > user clicks sound button
+// browser loads html > browser loads js > open the dialog > user closes dialog > audio system loads > user clicks sound button
 // find our dialog
-const introDialog = document.getElementById('intro-dialog');
+const introDialog = document.getElementById("intro-dialog");
 // find the close button
-const introDialogCloseButton = document.getElementById('intro-dialog-close');
+const introDialogCloseButton = document.getElementById("intro-dialog-close");
 // show the found element in our browser console
 // console.log(introDialog);
 // find our test button
 const testButton = document.getElementById('test-button');
 // init our synth
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth();
 
 ////// Dialog
 // show dialog on page load
-introDialog.showModal()
+introDialog.showModal();
 // close dialog when user clicks
 introDialogCloseButton.addEventListener("click", function closeIntroDialog() {
     introDialog.close();
 });
+// whenever dialog closes, initialise the audio system
+introDialog.addEventListener("close", toneInit);
 
 // we put the whole function inside the event listener instead as its only called there
 //function closeIntroDialog(){
 
 //}
 
+////// Tone
+// run to setup our audio system
+function toneInit(){
+    synth.connect(Tone.Destination);
+}
+
 // do something when this button is clicked
-testButton.addEventListener("click", playNote)
+testButton.addEventListener("click", playNote);
+
 // function that runs when button is clicked
 function playNote(){
-    // create a note for a duration
+    //play a note for a duration
     synth.triggerAttackRelease("c4", "8n");
 }
