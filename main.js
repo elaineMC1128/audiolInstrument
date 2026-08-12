08bb69aeb7fce43f9c8c3e71cb2e77bd4f480d36
s4151128 <s4151128@student.rmit.edu.au>
2026-08-12 14:50:53 +1000

    add dialog on page load

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+// browser loads html > browser loads js > open the dialog > user close dialog > audio system loads > user clicks sound button
+// find our dialog
+const introDialog = document.getElementById('intro-dialog');
+// find the close button
+const introDialogCloseButton = document.getElementById('intro-dialog-close');
+// show the found element in our browser console
+// console.log(introDialog);
 // find our test button
-const testButton = document.getElementById("test-button");
+const testButton = document.getElementById('test-button');
 // init our synth
 const synth = new Tone.Synth().toDestination();
+
+////// Dialog
+// show dialog on page load
+introDialog.showModal()
+// close dialog when user clicks
+introDialogCloseButton.addEventListener("click", function closeIntroDialog() {
+    introDialog.close();
+});
+
+// we put the whole function inside the event listener instead as its only called there
+//function closeIntroDialog(){
+
+//}
 
 // do something when this button is clicked
 testButton.addEventListener("click", playNote)
